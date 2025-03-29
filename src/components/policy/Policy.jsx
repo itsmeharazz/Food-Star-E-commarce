05ab3524ca91c1bda8cad420cf36ad522c663e3c
[Meharazul islam] <[itsmeharazz@gmail.com]>
2025-03-29 15:25:11 +0600

    add policy section

---
+++ src/components/policy/Policy.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FaDollarSign } from "react-icons/fa6";
+import { FaGlobe } from "react-icons/fa";
+
+
+const Policy = () => {
+  return (
+    <div className='w-full mb-10 py-10  gird place-content-center bg-[#d9d9d9]'>
+      <div className='container w-full md:w-[50%] mx-auto block  md:flex justify-between items-center gap-4'>
+        <div className='flex items-center gap-3'>
+          <samp className='text-4xl text-[#FBAA72] font-bold'>⟳</samp>
+          <p className='sm:text-sm md:text-xl'>Cash on delivery</p>
+        </div>
+        <div className='flex items-center gap-3'>
+          <FaDollarSign className='text-3xl text-[#FBAA72] font-bold' />
+          <p className='sm:text-sm md:text-xl'>Cash on delivery</p>
+        </div>
+        <div className='flex items-center gap-3'>
+          <FaGlobe className='text-3xl text-[#FBAA72] font-bold' />
+          <p className='sm:text-sm md:text-xl'>Online support</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Policy
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Category from '../components/category/Category'
 import Flashsale from '../components/Offer/Flashsale';
 import Topsaleing from '../components/Topsaleing/Topsaleing';
 import Products from '../components/products/Products';
+import Policy from '../components/policy/Policy';
 const Home = () => {
   const [category, setCategory]=useState("All");
   return (
@@ -13,6 +14,7 @@ const Home = () => {
     <Flashsale />
     <Topsaleing />
     <Products />
+    <Policy />
     </div>
   )
 }
